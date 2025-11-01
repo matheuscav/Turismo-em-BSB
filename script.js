c2db0af9d41a542c8d06fb9176ebcbd5ab5b7539
Gabriela Martins <132864619+Gab-Martins@users.noreply.github.com>
2025-11-01 00:39:14 +0000

    correção de bugs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const galeriaImagens = document.querySelectorAll('.galeria img');
 const overlay = document.querySelector('.overlay');
 const overlayImagem = document.getElementById('overlay-imagem');
+const menuHamburguer = document.getElementById('menu-hamburguer');
+const menuNavegacao = document.getElementById('menu-navegacao');
 
 function abrirImagem(event) {
     const imagemClicadaSrc = event.target.src;
@@ -12,8 +14,15 @@ function fecharImagem() {
     overlay.style.display = 'none';
 }
 
-galeriaImagens.forEach(imagem => {
-    imagem.addEventListener('click', abrirImagem);
-});
+if (galeriaImagens.length > 0) {
+    galeriaImagens.forEach(imagem => {
+        imagem.addEventListener('click', abrirImagem);
+    });
+    overlay.addEventListener('click', fecharImagem);
+}
 
-overlay.addEventListener('click', fecharImagem);
+if (menuHamburguer) {
+    menuHamburguer.addEventListener('click', () => {
+        menuNavegacao.classList.toggle('aberto');
+    });
+}
